23b23ca6f01796144dd1a685d6c9c6aaf36b0ca1
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-24 17:08:08 -0300

    jogada especial promocao peao dama

--- xadrez-console/Xadrez/PartidaDeXadrez.cs
+++ xadrez-console/Xadrez/PartidaDeXadrez.cs
@@ -144,6 +144,22 @@ namespace XadrezNS
                 throw new TabuleiroException("Você não pode se colocar em Xeque!");
             }
 
+            Peca p = Tab.Peca(destino);
+
+            // # jogada especial Promocao
+            if (p is Peao)
+            {
+                if ((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    _pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    _pecas.Add(dama);
+                }
+            }
+
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -162,8 +178,6 @@ namespace XadrezNS
                 Turno++;
                 MudaJogador();
             }
-
-            Peca p = Tab.Peca(destino);
 
             //#jogada especial en passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
